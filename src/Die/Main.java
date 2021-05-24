0f209543e913f916d7688b39ea1ac480406359ce
Zarry Die <zarrydie03@gmail.com>
2021-05-23 21:38:26 -0700

    We made it to githup with intelJ

---
+++ src/Die/Main.java
@@ -0,0 +1,17 @@
+package Die;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //system.out.println("Hello From ").Die;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello From: ");
+        String myName = input.nextLine();
+        System.out.println("Hello, my name is: " + myName);
+        System.out.println("We want to be expert in java programming.");
+    }
+}
