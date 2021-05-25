ab051881cd03e638e8f8402d155fe54bcf42a8e1
PhabelSanchez <phabel.sanchez@gmail.com>
2021-05-24 20:25:02 -0600

    Space added on second joined string at Line 14 "What is your name?" question added

--- src/Die/Main.java
+++ src/Die/Main.java
@@ -11,7 +11,8 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello From: ");
         String myName = input.nextLine();
-        System.out.println("Hello, my name is: " + myName + "I love git");
+        System.out.println("Hello, my name is: " + myName + " I love git");
+        System.out.println("What is your name?");
         String teamName = input.nextLine();
         System.out.println("Hello " + teamName);
         System.out.println("my name is " + myName);
